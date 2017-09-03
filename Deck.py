a69359c03f5362e81078ce739cae8bfa1c4731e6
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-09-03 18:10:43 -0500

    deals cards and stores them in a list

--- Deck.py
+++ Deck.py
@@ -10,34 +10,33 @@ class Deck:
         # stored as key-value pairs
         deck = {'Ace of Spades': 1, '2 of Spades': 2, '3 of Spades': 3, '4 of Spades': 4,
                 '5 of Spades': 5, '6 of Spades': 6, '7 of Spades': 7, '8 of Spades': 8,
-                '9 of Spades': 9, '10 of Spades': 10, 'Jack of Spades': 10, 'Queen of Spades': 10,
-                'King of Spades': 10,
+                '9 of Spades': 9, '10 of Spades': 10, 'Jack of Spades': 11, 'Queen of Spades': 12,
+                'King of Spades': 13,
 
                 'Ace of Hearts': 1, '2 of Hearts': 2, '3 of Hearts': 3, '4 of Hearts': 4,
                 '5 of Hearts': 5, '6 of Hearts': 6, '7 of Hearts': 7, '8 of Hearts': 8,
-                '9 of Hearts': 9, '10 of Hearts': 10, 'Jack of Hearts': 10, 'Queen of Hearts': 10,
-                'King of Hearts': 10,
+                '9 of Hearts': 9, '10 of Hearts': 10, 'Jack of Hearts': 11, 'Queen of Hearts': 12,
+                'King of Hearts': 13,
 
                 'Ace of Diamonds': 1, '2 of Diamonds': 2, '3 of Diamonds': 3, '4 of Diamonds': 4,
                 '5 of Diamonds': 5, '6 of Diamonds': 6, '7 of Diamonds': 7, '8 of Diamonds': 8,
-                '9 of Diamonds': 9, '10 of Diamonds': 10, 'Jack of Diamonds': 10, 'Queen of Diamonds': 10,
-                'King of Diamonds': 10,
+                '9 of Diamonds': 9, '10 of Diamonds': 10, 'Jack of Diamonds': 11, 'Queen of Diamonds': 12,
+                'King of Diamonds': 13,
 
                 'Ace of Clubs': 1, '2 of Clubs': 2, '3 of Clubs': 3, '4 of Clubs': 4,
                 '5 of Clubs': 5, '6 of Clubs': 6, '7 of Clubs': 7, '8 of Clubs': 8,
-                '9 of Clubs': 9, '10 of Clubs': 10, 'Jack of Clubs': 10, 'Queen of Clubs': 10,
-                'King of Clubs': 10}
+                '9 of Clubs': 9, '10 of Clubs': 10, 'Jack of Clubs': 11, 'Queen of Clubs': 12,
+                'King of Clubs': 13}
 
-        # creates a list of deck keys
-       # shuffled_deck = [deck.keys()]
 
-        # shuffles list of keys
-       # random.shuffle(shuffled_deck)
-
-        # returns the shuffled deck
         return deck
 
     def deal_card(self, deck, number):
+        # creates a list of deck keys
+        shuffled_deck = list(deck.keys())
+
+        # shuffles list of keys
+        random.shuffle(shuffled_deck)
 
         # accumulator for hand
         hand_value = 0
@@ -48,11 +47,16 @@ class Deck:
 
         # deals the cards and their values
         for count in range(number):
-            card, value = deck.popitem()
+            #grabs a random key from the list
+            card = shuffled_deck.pop()
+            #uses that key to get the value of the card
+            value = deck.pop(card)
             hand_value += value
-            print(card)
+            return value, card
+
 
         # print("Your hand: " + hand_value)
+
 
 
 
--- Game.py
+++ Game.py
@@ -1,13 +1,32 @@
 import Deck
+#determines which 'person' gets to keep the card in each round
+def turnWinner(playerValue, dealerValue, playerCard, dealerCard):
+    if playerValue > dealerValue:
+        playerList.append(playerCard)
+    elif playerValue < dealerValue:
+        dealerList.append(dealerCard)
+
+
 
 # deck instance
 deck = Deck.Deck()
-
+dealerList = []
+playerList = []
 # creates a new deck object
 new_deck = deck.create_deck()
 
 # first parameter asks for deck dictionary, second asks for how many cards to be dealt then prints keys
-deck.deal_card(new_deck, 7)
+#deck.deal_card(new_deck, 7)
+
+#deals for player and dealers and returns value and card
+playerValue, playerCard = deck.deal_card(new_deck, 1)
+dealerValue, dealerCard = deck.deal_card(new_deck, 1)
+turnWinner(playerValue, dealerValue, playerCard, dealerCard)
+
+print(playerCard)
+print(dealerCard)
+print(playerList)
+print(dealerList)
 
 
 
