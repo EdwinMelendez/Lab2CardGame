e40942608e1f564bbc2111a0b4e42bd6fcd72da6
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-09-06 13:50:41 -0500

    one error left but otherwise the game works and displays cards and all that

--- Game.py
+++ Game.py
@@ -8,9 +8,12 @@ def turnWinner(playerValue, dealerValue, playerCard, dealerCard):
 
     if playerValue > dealerValue:
         playerList.extend((playerCard, dealerCard))
+        print("You win this battle!")
     elif playerValue < dealerValue:
         dealerList.extend((dealerCard, playerCard))
+        print("Retreat!")
     elif playerValue == dealerValue:
+        print("Both sides are locked in a head to head battle, make a move!")
         tieBreaker()
 
 
@@ -28,13 +31,16 @@ def tieBreaker():
         # deals two more cards to determine turn winner
         playerValue, playerCard = deck.deal_card(new_deck, 1)
         dealerValue, dealerCard = deck.deal_card(new_deck, 1)
+        print("You drew a " + playerCard + " Your opponent drew a " + dealerCard)
         # adds 8 cards to the winner
         if playerValue > dealerValue:
             playerList.extend((playerCard, dealerCard))
             playerList.extend(extraList)
+            print("You have won this battle! Take all the prisoners! (aka cards")
         elif playerValue < dealerValue:
             dealerList.extend((dealerCard, playerCard))
             dealerList.extend(extraList)
+            print("Your enemy has outflanked you! Run away!")
         elif playerValue == dealerValue:
             tieBreaker()
     #handles if there are not enough cards to play a normal tie breaker
@@ -55,27 +61,32 @@ def tieBreaker():
         extraList = []
         addingTieBreakCards(extraList)
         print("two")
+
 #odd tie breaker function
 def addingTieBreakCards(extraList):
     extraList = extraList
     playerValue, playerCard = deck.deal_card(new_deck, 1)
     dealerValue, dealerCard = deck.deal_card(new_deck, 1)
+    print("You drew a " + playerCard + " Your opponent drew a " + dealerCard)
     if playerValue > dealerValue:
         playerList.extend((playerCard, dealerCard))
         playerList.extend(extraList)
+        print("You have secured a narrow victory")
     elif playerValue < dealerValue:
         dealerList.extend((dealerCard, playerCard))
         dealerList.extend(extraList)
+        print("The enemy has you pinned down...time to surrender")
     elif playerValue == dealerValue:
-        print("Its a draw! These cards will be discarded")
+        print("This battle has no victors")
+
 #determines the winner of the game
 def gameWinner():
     if len(dealerList) > len(playerList):
-        print("You lose")
+        print("You have lost this War..surrender in shame")
     elif len(playerList) > len(dealerList):
-        print("You win")
+        print("You have emerged victorious in this War!")
     elif len(playerList) == len(dealerList):
-        print("tie")
+        print("This War has resulted in a stalemate...there are no winners")
 
     # deck instance
 deck = Deck.Deck()
@@ -83,15 +94,17 @@ dealerList = []
 playerList = []
     # creates a new deck object
 new_deck = deck.create_deck()
+choose = str("Press enter to begin your game of War")
+input(choose)
 
-# first parameter asks for deck dictionary, second asks for how many cards to be dealt then prints keys
-# deck.deal_card(new_deck, 7)
 while len(new_deck) > 0:
+    choose = str("Press enter to attack")
+    input(choose)
     # deals for player and dealers and returns value and card
     playerValue, playerCard = deck.deal_card(new_deck, 1)
     dealerValue, dealerCard = deck.deal_card(new_deck, 1)
+    print("You drew a " + playerCard + " Your opponent drew a " + dealerCard)
+
     turnWinner(playerValue, dealerValue, playerCard, dealerCard)
 gameWinner()
 
-print(dealerList)
-print(playerList)
